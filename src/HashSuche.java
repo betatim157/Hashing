1be8ca2086111db8ccaf5d155ba4513cb26102be
46B122U12 <tim.steuerr@gmail.com>
2022-06-02 11:20:03 +0200

    test

--- src/HashSuche.java
+++ src/HashSuche.java
@@ -1,4 +1,5 @@
 public class HashSuche {
+    //Attribute für Array und Schlüssel
     private final int[] zahlen = new int [10];
     private final double[] zahlen1 = new double[10];
     private final int mod = 10;
@@ -9,36 +10,64 @@ public class HashSuche {
     public void berechneHash(){
 
     }
-
+    //Fügt einen Wert ein
     public int fuegeEin(int pSchlüssel,double wert){
      int k = 0;
      int z;
-     z = pSchlüssel%mod;
+     z = pSchlüssel%mod; //Rest wird berechnet und danach sortiert
      if(zahlen[z] == 0)
      {
-         zahlen[z] = pSchlüssel;
+         zahlen[z] = pSchlüssel; //Schlüssel wird direkt eingefügt da dort noch kein Wert leigt
          zahlen1[z] = wert;
      }else
      {
-        for(int i = 1; i< zahlen.length; i++)
+        for(int i = 0; i< zahlen.length-1; i++)
         {
-            k++;
-            if(zahlen[z+i] == 0)
+         if(i!=9)
             {
-                zahlen[z+i] = pSchlüssel;
-                zahlen1[z+i] = wert;
-            }
-            if(i==9){
-                System.out.println("Alle Felder sind belget.");
-            }
-
+                k++;
+                if(zahlen[z+i] == 0)
+                {
+                    zahlen[z + i] = pSchlüssel;
+                    zahlen1[z + i] = wert;
+                    return k;
+                }
+            }else{
+             return -1;
+         }
         }
      }
-     return k;
+     return 0;
     }
+
+    public int suchePositionVon(int pSchlüssel){
+       int b = -1; //Wenn -1 dann ausgeben wird heißt das der Schlüssel garnicht vorliegt
+       for(int i = 0; i<9; i++)
+       {
+           if(zahlen[i] == pSchlüssel)
+           {
+               return i; // Dort wurde dann die Stelle gefunden
+           }
+       }
+       return b;
+    }
+    public double gibWertVon(int pSchlüssel){ //ähnlich wie die vorherige Methode diesmal jedoch mit anderem Array und Wert wird ausgegeben
+       double b = -1; //Wenn -1 dann ausgeben wird heißt das der Schlüssel garnicht vorliegt
+        for(int i = 0; i <9; i++)
+        {
+            if(zahlen[i] == pSchlüssel)
+            {
+                return zahlen1[i];// Dort wurde dann die Stelle gefunden
+            }
+        }
+        return b;
+    }
+
     public void showarray(){
         System.out.println(java.util.Arrays.toString(zahlen));
         System.out.println(java.util.Arrays.toString(zahlen1));
        }
+
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,20 @@ import javax.swing.*;
 
 public class Main{
     public static void main(String[] args) {
-    System.out.println("Hello world!");
     HashSuche s = new HashSuche();
-    Systems.fuegeEin(4356939, 10.5);
-    s.
+    s.fuegeEin(22, 10.5);
+    s.fuegeEin(12324,6);
+    System.out.println(s.fuegeEin(13456,73));
+    System.out.println(s.fuegeEin(10,2));
+    System.out.println(s.fuegeEin(13456,3));
+    System.out.println(s.fuegeEin(1341236,93));
+    System.out.println(s.fuegeEin(131412556,13));
+    System.out.println(s.fuegeEin(1345612312,223));
+    System.out.println(s.fuegeEin(13412256,23));
+    System.out.println(s.fuegeEin(134145656,55));
+
+
+    s.showarray();
 
 
     }
